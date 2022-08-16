7fbfba1dfd3fafff31f9c8e57393e11148dc07fc
Jordan Yamada <sadaotomokane@gmail.com>
2022-08-16 16:53:18 -0700

    added more images, votes feature, and bootstrap card

--- collab.md
+++ collab.md
@@ -1,1 +1,7 @@
+# Collaborations
+
+## Lab01
 Had help from Jeffrey Smith for Lab01-initial React.
+
+## Lab02
+Worked with Amy Pierce, Jeffrey Smith, Alfredo Orquiz
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,41 @@
 import React from "react";
+import './HornedBeast.css';
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      votes: 0,
+    }
+  }
+
+  handleVotes = () => {
+    this.setState({
+      votes: this.state.votes +1,
+    });
+  };
+
   render() {
     return (
-      <article>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description}/>
-        <p>{this.props.decription}</p>
-      </article>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={this.props.image_url} alt={this.props.description}/>
+      <Card.Body>
+      <p>💖 {this.state.votes}</p>
+        <Card.Title>{this.props.title}</Card.Title>
+        <Card.Text>
+        {this.props.description}
+        </Card.Text>
+        <p onClick={this.handleVotes}>Votes!</p>
+      </Card.Body>
+    </Card>
+      // <article>
+      //   <h2>{this.props.title}</h2>
+      //   <p>💖 {this.state.votes}</p>
+      //   <img src={this.props.image_url} alt={this.props.description}/>
+      //   <p>{this.props.description}</p>
+      //   <p onClick={this.handleVotes}>Vote!</p>
+      // </article>
     )
   }
 }
